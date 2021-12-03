16e9ebd4dcb53305c520d7c45b319d6f4ce03230
Kevin-CMD10 <94182931+Kevin-CMD10@users.noreply.github.com>
2021-12-03 10:27:28 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var deButton = document.querySelector("nav:nth-of-type(1) button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  var deHeader = document.querySelector("header");
+  deHeader.classList.toggle("toonMenu");
+}
