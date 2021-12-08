3e3e6c0cc824ea092f17021e55875466eae15687
Kevin-CMD10 <94182931+Kevin-CMD10@users.noreply.github.com>
2021-12-08 12:56:52 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var deButton = document.querySelector("nav:nth-of-type(1) button");
+var deButton = document.querySelector(".home nav:nth-of-type(1) button");
 
 deButton.addEventListener("click", toggleMenu);
 
@@ -7,3 +7,13 @@ function toggleMenu(event) {
   var deHeader = document.querySelector("header");
   deHeader.classList.toggle("toonMenu");
 }
+
+
+// var hamburger = document.querySelector(".bestellingen nav:nth-of-type(1) button");
+
+// hamburger.addEventListener("click", toggleMenu);
+
+// function toggleMenu(event) {
+//   var deHamburger = document.querySelector("header");
+//   deHamburger.classList.toggle("toonMenu");
+// }
